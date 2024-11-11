6707714e9b493b55d45b07820a04c4421c2637d2
faris-code0317 <farissmk@gmail.com>
2024-11-11 09:27:36 +0700

    perbaikan login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no shrink-to-fit=no">
     <title>Aplikasi Kesehatan | Data Data Pasien Siswa/siswi</title>
     <meta name="description"
-        content="Website yang menyediakan konten konten islami seperti Al-quran, Sunnah Nabi, Doa Sehari Hari, dll. Dengan desain yang sederhana dan mudah dinavigasi, website ini bertujuan untuk memberikan akses yang lebih luas kepada umat Islam dalam mempelajari, memahami, dan mengamalkan ajaran Islam." />
-    <meta name="keywords" content="majmu syarif, Irsyadut Thulab, web islami, doa doa, doa sehari hari, alquran">
+        content="" />
+    <meta name="keywords" content="">
     <meta name="robots" content="index, follow">
     <link rel="icon" href="{{ asset('image/logo.png') }}" type="image/x-icon">
     <style>
--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::middleware(['auth'])->group(function(){
     Route::get('/logout',[SesiController::class,'logout']);
 
 });
-Route::get('home',[SesiController::class, 'landing'])->name('landing');
+Route::get('homelanding',[SesiController::class, 'landing'])->name('landing');
 
 Route::get('ukssiswa', [UKSController::class, 'index'])->name('ukssiswa');
 Route::get('tmbhsiswa', [UKSController::class, 'tambah'])->name('tmbhsiswa');
